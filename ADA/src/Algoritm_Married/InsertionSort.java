c884f8042d82f8feb5e93085da90a026b62e59dd
EDSEL <ealvan@unsa.edu.pe>
2020-10-01 13:56:37 -0500

    comentarios para Insertion and MergeSort

--- ADA/src/Algoritm_Married/InsertionSort.java
+++ ADA/src/Algoritm_Married/InsertionSort.java
@@ -1,35 +1,56 @@
 package Algoritm_Married;
 
+import java.util.Arrays;
 
 public class InsertionSort {
 
 	public static void main(String[] args) {
-	for(int i = 1 ; i < 4501; i++) {
+	//generaremos arrays de 1 hasta 100 elementos
+	for(int i = 1 ; i < 100; i++) {
+		//Generando el array que es aleatorio
 		int[] list = generate(i);
-		list = insertionSort(list);
-		reverse(list);
+		//ordenando por insertionSort la lista
+		//list = insertionSort(list);
+		//ahora lo mas importante, es hacer que sea 
+		//el peor caso del insertionSort
+		//(el peor es cuando esta ordenado al reves)
+		//reverse(list);
+		//ahora podemos tener menos latencia
 		long start = System.nanoTime();
 		list = insertionSort(list);
+		//obteniendo el tiempo
 		long time = System.nanoTime() - start;
+		//imprimir el tiempo
 		System.out.println(time);
 	}
-	
-	}
+}
+//el metodo insertionSort lo que hace es ordenar una lista
+//intercambiando los elementos que no cumplan con el orden establecido
 public static int[] insertionSort(int[] list) {
+	
 	int index;
+	//recorremos desde el 1 hasta n-1(n el es #elementos)
 	for(int i = 1; i < list.length; i++) {
+		//obteniendo el indice presente
 		index = list[i];
+		//ahora vamos a ver si alguno no cumple con el orden
+		//todos los elementos desde 0 hasta i-1
 		for(int j = 0; j < i; j++) {
+			//si uno es mayor que el index
 			if(index < list[j]) {
+				//lo intercambiamos en su lugar
 				int aux = list[i];
 				list[i] = list[j];
 				list[j] = aux;
 			}
 		}
 	}
+	//retornamos la lista
 	return list;
 }
+//recibe una lista y la convierte en una lista al reves
 public static void reverse(int[] list) {
+	//creando una lista con el mismo #elelemntos
 	int [] list_ = new int[list.length];
 	int i = 0;
 	for(Integer key: list) {
@@ -39,6 +60,7 @@ public static void reverse(int[] list) {
 	list = list_;
 	
 }
+//esta imprime una lista
 public static void print_(int[] list) {
 	String str = "";
 	for(int i = 0; i < list.length; i++) {
@@ -46,11 +68,18 @@ public static void print_(int[] list) {
 	}
 	System.out.println(str);
 }
+//este metodo dado un "num" de elementos nos devuelve una lista 
+//con "num" elementos de la lista
 public static int[] generate(int num) {
+	//lista con el numero de elementos
 	int[] list = new int[num];
+	//creando elemento por elemento para la lista
 	for(int i = 0; i < list.length; i++) {
-		list[i] = (int)(Math.random()*num + 1);	
+		//(el numero aleatorio esta entre [1,num])
+		list[i] = num - i ;	
 	}
+	//retornamos la lista
 	return list;
 }
+
 }
--- ADA/src/Algoritm_Married/MergeSort.java
+++ ADA/src/Algoritm_Married/MergeSort.java
@@ -4,29 +4,25 @@ package Algoritm_Married;
 public class MergeSort {
 
 	public static void main(String[] args) {
-		
-		//long start = System.currentTimeMillis();
-		//int[] list = {1,2,0,0,0};
-		//int[] min = {-1,4,3};
-		//int[] list1 = {1};
-		//list1 = rest(list1);
-		//print_(list1);
-		//System.out.print(list1.length);
-		//list = append(min,list);
-		//min = mergeSort(min);
-		//long fin = System.currentTimeMillis() - start;
-		//System.out.println(fin);
-		//print_(min);
-		
+		//esta sera la lista
 		int[] list;
+		//declaramos el inicio y el final del algoritmo
 		long start, fin ;
-		for(int i = 1; i < 10000; i++) {
+		//ahora vamos a generar las listas e imprimir el tiempo
+		for(int i = 1; i < 100; i++) {
+			//generar las listas
 			list  = generate(i);
-			list = mergeSort(list);
-			reverse(list);
+			//aplicar el mergeSort
+			//list = mergeSort(list);
+			//ahora ponerlo al reves, que es el peor caso
+			//reverse(list);
+			//ahora vamos a empezar el reloj
 			start = System.nanoTime();
+			//ahora es donde mediremos verdaderamente el algoritmo
 			list = mergeSort(list);
+			//ahora capturamos el tiempo
 			fin = System.nanoTime() - start;
+			//Y lo imprimimos
 			System.out.println(fin);
 		}
 	}
@@ -47,89 +43,134 @@ public static void print_(int[] list) {
 	}
 	System.out.println(str);
 }
+//el mergeSort recibe un array , el cual se ordenara partiendolo y ordenandolo
 public static int[] mergeSort(int arr[]) {
+	//si el array esta en su minima expresion
+	//(un solo elemento) que es el caso base
 	if(arr.length <= 1) {
+		//returnamos
 		return arr;
 	}
-	int left[] = new int[arr.length/2];
-	int right[] = new int[arr.length-arr.length/2];
-	int  _result_[] = new int[left.length+right.length];
+	//ahora para lograr llegar a su minima expresion debemos partirlos
+	//la primera mitad a la izq
+	int izq[] = new int[arr.length/2];
+	//la segunda mitad a la derecha
+	int dere[] = new int[arr.length-arr.length/2];
+	//esta lista nos dara la lista ordenada, cada vez que retorne
+	int  _result_[] = new int[izq.length+dere.length];
+	//ahora vamos a llenar las listas "izq" y la lista "der"
+	//hallamos la mitad
+	int mitad = arr.length/2;
 	
-	int middle = arr.length/2;
-	//0 ..to.. middle - 1
-	for(int i = 0; i < middle; i++) {
-		left[i] = arr[i];
+	//llenando la lista hasta mitad - 1
+	//0 .... mitad - 1
+	for(int i = 0; i < mitad; i++) {
+		izq[i] = arr[i];
 	}
-	for(int i = middle; i < arr.length; i++) {
-		right[i - middle] = arr[i];
+	//mitad ...... arr.length
+	for(int i = mitad; i < arr.length; i++) {
+		dere[i - mitad] = arr[i];
 	}
-	left = mergeSort(left);
-	right = mergeSort(right);
-	if (left[left.length - 1 ] <= right[0]) {
-		//int  _result_[] = new int[left.length+right.length];
+	//ahora que ya partimos la lista podemos partirlas de nuevo
+	//hasta llegar el caso base, que es 1 elemento
+	izq = mergeSort(izq);
+	dere = mergeSort(dere);
+	//el siguiente codigo se leera desde que se active el "return arr"
+	//cuando se active el caso mas simple es de dos array, y cada uno 
+	//con un solo elemento
+	//es por eso que cuando se active el siguiente if, valdra para cualquier array
+	//que al menos tenga un elemento
+	if (izq[izq.length - 1 ] <= dere[0]) {
+ 		//ahora cuando la condicion se cumpla significa que los dos array
+		//estan ordenados entre si, significa que solo queda juntarlos 
+		//en el array result
 		for(int i = 0; i < _result_.length; i++) {
-			if(i < left.length) {
-				_result_[i] = left[i];
+			if(i < izq.length) {
+				_result_[i] = izq[i];
 			}else {
-				_result_[i] = right[i-middle];
+				_result_[i] = dere[i-mitad];
 			}
 		}
+		//lo retornamos
 		return _result_;
 	}
-	_result_ = merge(left,right);
+	//pero si es que esto no se cumple significa que no estan ordenados entre si
+	//por lo que necesitamos ordenarlos y UNIRLOS
+	_result_ = union(izq,dere);
 	return _result_;
 }
-public static int[] merge(int left[],int right[]) {
-	int[] result=new int[right.length+left.length];
-
+//este metodo recibe dos array desordenados entre si
+//los ordena, y despues los une en un array y lo retorna
+public static int[] union(int izq[],int dere[]) {
+	//preparamos un array para recibir la union ordenada de los dos array
+	int[] result=new int[dere.length+izq.length];
+	//este i, iterara sobre result
 	int i = 0;
-	
-	while(left.length > 0 && right.length > 0) {
-		if( left[0] <= right[0] ) {
-			result[i] = left[0];
-			left = rest(left);
+	//ahora este bucle se seguira ejecutando, hasta que uno o los dos
+	//esten arrays esten vacios
+	while(izq.length > 0 && dere.length > 0) {
+		//comparamos cada primer elemento de las dos listas
+		//si el 1er elemento de la lista "izq" es menor
+ 		if( izq[0] <= dere[0] ) {
+			//ahora lo agregamos el elemento en "result"
+			result[i] = izq[0];
+			//y ahora lo eliminamos del  1er elemento
+			izq = restar(izq);
 		}else{
-			result[i] = right[0];
-			right = rest(right);
+			//de los contrario el 1er elemento de la lista "dere"
+			//es mayor, entonces lo agregamos
+			result[i] = dere[0];
+			//ahora lo elminamos
+			dere = restar(dere);
 		}
+ 		//aumentamos i, para que no sobreescriva el array result
 		i++;
 	}
-	if(left.length > 0) {
-		result = append(left,result);
+	//despues cuando se corte el bucle
+	//puede que el array "izq" tenga elementos aun
+	if(izq.length > 0) {
+		//entonces solo queda agregarlos a la lista result
+		result = agregar(izq,result);
 	}
-	if(right.length > 0) {
-		result = append(right,result);
+	//o el array "dere" tenga elementos
+	if(dere.length > 0) {
+		//entonces agregaomns los elemento restantes a la lista result
+		result = agregar(dere,result);
 	}
+	//y finalmente lo devolvemos ya ordenado y unido
 	return result;
 }
-public static int[] rest(int [] list) {
+//este metodo elimina el primer elemento de la lista
+public static int[] restar(int [] list) {
 	int []_list_ = new int[list.length-1];
 	for(int i = 1; i < list.length; i++) {
 		_list_[i-1] = list[i];  
 	}
+	//retornamos la lista
 	return _list_;
 }
-public static int[] append(int list_min[], int result[] ) {
+//este metodo agregar, agrega la lista "lista_min" a la lista "result" 
+public static int[] agregar(int list_min[], int result[] ) {
 	int i= 0;
+	//primero obtenemos desde que indice
+	//esta vacio
 	for(; i < result.length; i++) {
 		if(result[i] == 0) {
 			break;
 		}
 	}
+	//y luego lo llenamos
 	for(int j = i; j < result.length; j++ ) {
 		result[j] = list_min[j-i]; 
 	}
-	
+	//y lo retornamos
 	return result;
 }
 public static int[] generate(int num) {
 	int[] list = new int[num];
 	for(int i = 0; i < list.length; i++) {
-		list[i] = (int)(Math.random()*num + 1);	
+		list[i] = num - i;	
 	}
 	return list;
 }
-
-
-	
 }
