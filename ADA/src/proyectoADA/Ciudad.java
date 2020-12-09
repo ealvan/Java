2e97f598cfba18c97e353d646f1294d74e778a7e
EDSEL <ealvan@unsa.edu.pe>
2020-12-08 22:03:02 -0500

    commit

---
+++ ADA/src/proyectoADA/Ciudad.java
@@ -0,0 +1,20 @@
+package proyectoADA;
+
+public class Ciudad {
+
+	String nombre;
+	int id;
+	public Ciudad(String name, int id) {
+		this.id = id;
+		nombre = name;
+	}
+	public String toString() {
+		return nombre;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
